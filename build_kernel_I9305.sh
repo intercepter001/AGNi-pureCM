6af66dbee0e02f1bfd70fa0949ca6bee7fc9b845
psndna88 <psndna88@gmail.com>
2013-06-06 22:22:28 +0530

    added I9305 and N7105 support

---
+++ build_kernel_I9305.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+export KERNELDIR=`readlink -f .`
+CROSS_COMPILE=/Working_Directory/android_prebuilt/linux-x86/toolchain/arm-eabi-4.4.3/bin/arm-eabi-
+
+if [ ! -f $KERNELDIR/.config ];
+then
+  make defconfig psn_i9305_oc_v2.2_defconfig
+fi
+
+. $KERNELDIR/.config
+
+export ARCH=arm
+
+cd $KERNELDIR/
+nice -n 10 make -j4 || exit 1
+
+mkdir -p $KERNELDIR/BUILT_I9305/lib/modules
+
+rm $KERNELDIR/BUILT_I9305/lib/modules/*
+rm $KERNELDIR/BUILT_I9305/zImage
+
+find -name '*.ko' -exec cp -av {} $KERNELDIR/BUILT_I9305/lib/modules/ \;
+/Working_Directory/android_prebuilt/linux-x86/toolchain/arm-eabi-4.4.3/bin/arm-eabi-strip --strip-unneeded $KERNELDIR/BUILT_I9305/lib/modules/*
+cp $KERNELDIR/arch/arm/boot/zImage $KERNELDIR/BUILT_I9305/
+
---
+++ build_kernel_N7105.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+export KERNELDIR=`readlink -f .`
+CROSS_COMPILE=/Working_Directory/android_prebuilt/linux-x86/toolchain/arm-eabi-4.4.3/bin/arm-eabi-
+
+if [ ! -f $KERNELDIR/.config ];
+then
+   make defconfig psn_n7105_oc_v2.2_defconfig
+fi
+
+. $KERNELDIR/.config
+
+export ARCH=arm
+
+cd $KERNELDIR/
+nice -n 10 make -j4 || exit 1
+
+mkdir -p $KERNELDIR/BUILT_N7105/lib/modules
+
+rm $KERNELDIR/BUILT_N7105/lib/modules/*
+rm $KERNELDIR/BUILT_N7105/zImage
+
+find -name '*.ko' -exec cp -av {} $KERNELDIR/BUILT_N7105/lib/modules/ \;
+/Working_Directory/android_prebuilt/linux-x86/toolchain/arm-eabi-4.4.3/bin/arm-eabi-strip --strip-unneeded $KERNELDIR/BUILT_N7105/lib/modules/*
+cp $KERNELDIR/arch/arm/boot/zImage $KERNELDIR/BUILT_N7105/
+
